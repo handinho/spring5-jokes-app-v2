4097437d2bb602faa1a1c854926cd050fcc2ca2e
Marko Jovanovic <mjovanovic@infoart.hr>
2021-11-27 20:30:10 +0100

    Init application

---
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5jokesappv2;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(Spring5JokesAppV2Application.class, args);
+
+		ChuckNorrisQuotes c = new ChuckNorrisQuotes();
+
+		System.out.println(c.getRandomQuote());
+		System.out.println(c.getRandomQuote());
+		System.out.println(c.getRandomQuote());
+
+	}
+
+}
